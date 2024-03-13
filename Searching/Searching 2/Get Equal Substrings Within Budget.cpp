29c36dcfec72b510c7aa34c74fe37b71a1bb068e
Sanjay <121040100+sanjay-1458@users.noreply.github.com>
2024-03-13 21:16:10 +0530

    Update Get Equal Substrings Within Budget.cpp

--- Searching/Searching 2/Get Equal Substrings Within Budget.cpp
+++ Searching/Searching 2/Get Equal Substrings Within Budget.cpp
@@ -20,3 +20,39 @@ public:
     }
 };
 ///////////////////////////////////////////////////////////////////////////////////////////////////
+class Solution {
+    bool check(int len,int val,string&s,string&t){
+        int i=0,j=0,n=s.size();
+        int sum=0;
+        while(j<n){
+            sum+=abs(s[j]-t[j]);
+            if(j-i+1<len){
+                j++;
+            }
+            else{
+                if(sum<=val){
+                    return true;
+                }
+                sum-=abs(s[i]-t[i]);
+                i++;
+                j++;
+            }
+        }
+        return false;
+    }
+public:
+    int equalSubstring(string s, string t, int mx) {
+        int n=s.size();
+        int low=0,high=n;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            if(check(mid,mx,s,t)){
+                low=mid+1;
+            }
+            else{
+                high=mid-1;
+            }
+        }
+        return high==-1?0:high;
+    }
+};
